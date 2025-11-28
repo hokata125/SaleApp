eacb5cb7be1eaf05e2b499719c6234e31d80aa84
taehwangkhang <princekhang125@gmail.com>
2025-11-29 00:59:43 +0700

    BTH3 completed

--- saleappv2/eapp/index.py
+++ saleappv2/eapp/index.py
@@ -1,19 +1,28 @@
 from flask import Flask, render_template, request, redirect
-from flask_login import login_user
+from flask_login import login_user, logout_user, current_user
 from werkzeug.utils import redirect
 from eapp import app, dao, login
 from eapp.dao import auth_user
+import math
 
 
 @app.route('/')
 def index():
-    categories = dao.load_categories()
     products = dao.load_products(cate_id=request.args.get('category_id'),
                                  kw=request.args.get('kw'),
-                                 page=request.args.get('page'))
+                                 page=int(request.args.get('page', 1)))
 
-    return render_template('index.html',
-                           categories=categories, products=products)
+    return render_template('index.html',products=products,
+                           pages=math.ceil(dao.count_products()/app.config["PAGE_SIZE"]))
+                            #Làm tròn lên số trang
+
+@app.route('/login')
+def login_view():
+    return render_template('login.html')
+
+@app.route('/register')
+def register_view():
+    return render_template('register.html')
 
 @app.route('/login', methods=['post'])
 def login_process():
@@ -23,11 +32,25 @@ def login_process():
     if user:
         login_user(user=user)
 
-    return redirect('/admin')
+    next = request.args.get('next')
+    return redirect(next if next else'/admin')
+
+@app.route('/logout')
+def logout_process():
+    logout_user()
+    return redirect('/login')
+
 
 @login.user_loader
 def load_user(id):
     return dao.get_user_by_id(id)
+
+'''Response chung khi ở trang đăng nhập/đăng ký/trang chủ đều hiện các danh mục sp'''
+@app.context_processor
+def common_response():
+    return {
+        'categories': dao.load_categories()
+    }
 
 if __name__ == '__main__':
     from eapp import admin
